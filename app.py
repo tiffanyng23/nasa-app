6245b6cb50ce29c28a51b58b13508e2630b57f43
tiffanyng23 <ngtiffany23@gmail.com>
2026-02-26 22:35:55 -0500

    starting to build earth page

--- app.py
+++ app.py
@@ -4,13 +4,43 @@ import requests
 
 app = Flask(__name__)
 
-# homepage where you select to explore the earth or space
+# Homepage
 @app.route("/")
 def explore():
     return render_template("explore.html")
 
+# EARTH
 # route to EONET to track global natural events
+@app.route("/earth")
+def earth_events():
+    key = "Ow6qt32sEB5V8P4k7xMFluOM2cEEyGyTrRgQ7B3P"
+    date = datetime.today().strftime("%Y-%m-%d")
+    start = (datetime.today() - timedelta(weeks=1)).strftime("%Y-%m-%d")
 
+    # get earth events from the past week
+    try:
+        response = requests.get(url = "https://eonet.gsfc.nasa.gov/api/v3/events", params={"start": start, "end": date, "api_key":key})
+        data = response.json()
+        events_data = data["events"]
+        print(f"EONET Request: {response.status_code}")
+    except requests.exceptions.HTTPError as http_error: #when http request returns an error
+        print(f"HTTP Error: {http_error}")
+    except requests.exceptions.RequestException as error: #all other errors
+        print(f"Error: {error}")
+
+    #extract data: Category ID, Title, Date, Coordinates, Magnitude Value
+    earth_events={}
+    for event in events_data:
+        earth_events[event["id"]] = {"title": event["title"], 
+                                    "date": event["geometry"][0]["date"],
+                                    "magnitude": event["geometry"][0]["magnitudeValue"],
+                                    "type": event["categories"][0]["id"],
+                                    "coordinates": event["geometry"][0]["coordinates"]}
+
+    return render_template("earth.html")
+
+
+# SPACE
 # route to astronomy pictures
 @app.route("/space")
 def weekly_images():
@@ -30,6 +60,5 @@ def weekly_images():
 
     #reverse order of images so it displays as most recent first
     images = dict(reversed(images_data.items()))
-
 
     return render_template("space.html", images_data = images)
