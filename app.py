b891988a56cf252a030295eec1c9f62a3ebc09f1
tiffanyng23 <ngtiffany23@gmail.com>
2026-02-28 21:18:04 -0500

    built apod explore page

--- app.py
+++ app.py
@@ -14,13 +14,19 @@ import pytextrank
 
 app = Flask(__name__)
 
+# HOMEPAGE
+@app.route("/")
+def explore():
+    return render_template("explore.html")
+
+
 # SPACE
 # route to astronomy pictures
-@app.route("/")
+@app.route("/space")
 def weekly_images():
     key = "Ow6qt32sEB5V8P4k7xMFluOM2cEEyGyTrRgQ7B3P"
     date = datetime.today().strftime("%Y-%m-%d")
-    start = (datetime.today() - timedelta(weeks=1)).strftime("%Y-%m-%d")
+    start = (datetime.today() - timedelta(days=10)).strftime("%Y-%m-%d")
 
     #astronomy picture of the day
     try:
@@ -39,20 +45,29 @@ def weekly_images():
     for img in data:
 
         #extract other data for each image
-        images_data[img["date"]] = {"title": img["title"], "image": img["url"], "caption": img["explanation"], "keywords":[]}
+        images_data[img["date"]] = {"title": img["title"], "image": img["url"], "caption": img["explanation"], "keywords":{}}
 
-        # using TestRank for keyword extraction from image description
+        # NLP with PyTestRank for keyword extraction from image description
         #identify keywords from description to use in NASA image/video library
-        text = img["explanation"]
+        text = ": ".join([img["title"],img["explanation"]])
         nlp = spacy.load("en_core_web_sm")
         nlp.add_pipe("textrank")
         summary = nlp(text)
 
         #save each keyword extracted from pytextrank in a list (new list for each image)
-        for phrase in summary._.phrases[:5]:
-            images_data[img["date"]]["keywords"].append(phrase.text)
+        for phrase in summary._.phrases[:8]:
+            #extract top 8 keywords from title + description
+            word = phrase.text
+
+            #remove quotes for word
+            url_word = word.strip('')
+            #create url for each keyword to go to NASAs image and video library
+            url = f"https://images.nasa.gov/search?q={url_word}&page=1&media=image,video,audio&yearStart=1920&yearEnd=2026"
+
+            #update dictionary with keyword:url key-value pair for each day
+            images_data[img["date"]]["keywords"][phrase.text] = url
 
     #reverse order of images so it displays as most recent first
     images = dict(reversed(images_data.items()))
 
-    return render_template("space.html", images_data = images)
+    return render_template("space.html", images_data = images, )
